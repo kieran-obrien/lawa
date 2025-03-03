ad609830e92d8fee862ea8cf40c4a1f8547e2d5f
Kieran O'Brien <kieranobrien741@gmail.com>
2025-03-03 21:11:08 +0000

    Add initial saving/loading funcs.

--- index.js
+++ index.js
@@ -1,13 +1,36 @@
 #!/usr/bin/env node
 
-import { program } from "commander";
+const { program } = require("commander");
+const fs = require("fs");
+const path = require("path");
+
+const vaultsJson = path.resolve(__dirname, "vaults.json");
+console.log(vaultsJson);
+
+function loadPaths() {
+    console.log("here?");
+    if (fs.existsSync(vaultsJson)) {
+        const data = fs.readFileSync(vaultsJson);
+
+        return JSON.parse(data).paths;
+    }
+    return [];
+}
+
+function saveVault(path, vaultName) {
+    vaults.push({ path: path, vaultName: vaultName });
+    fs.writeFileSync(vaultsJson, JSON.stringify({ paths: vaults }, null, 2));
+}
 
 program
     .version("1.0.0")
-    .description("My Node CLI")
+    .description("An Obsidian CLI helper tool.")
     .option("-n, --name <type>", "Add your name")
     .action((options) => {
         console.log(`Hey, ${options.name}!`);
     });
-
+const vaults = loadPaths();
+saveVault("testpath", "testname");
+console.log(vaults[0].path);
+//savePath("test/path");
 program.parse(process.argv);
